66bd5830df2886a2e0bcedf35c71525cd823d456
all1nol <newacckim@gmail.com>
2025-08-28 15:29:01 +0400

    Add new buffer allocation method and update comments in different-allocs.js

--- buffers/different-allocs.js
+++ buffers/different-allocs.js
@@ -4,8 +4,17 @@ const buffer = Buffer.alloc(10000,0)  //normal slow version
 
 const fastAlloc = Buffer.allocUnsafe(10000) //fast allocation
 
+const newBuffer = Buffer.allocUnsafeSlow(2) // new buffer created instead of already allocated buffer
+
 for(let i =0; i < fastAlloc.length; i++){
 if(fastAlloc[i] !== 0){
         console.log(`Elements at position ${i} has value: ${fastAlloc[i].toString("2")}`);  //take decimal and show it in binary 
     }
 }
+
+//safe option but still use allocUnsafe
+Buffer.from()
+Buffer.concat()
+
+
+ 
